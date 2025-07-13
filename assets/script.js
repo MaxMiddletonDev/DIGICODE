b557bfe8c006535ce71f58e4506a674382d0a47a
3trn <maxedwardmiddleton@gmail.com>
2025-07-13 01:23:19 +0100

    Added buttons to qr generator place.
    Added placeholder and error icon for QR
    
    Silly jokes said when inputting empty fields

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,11 @@
+var counter = 0;
+const input = document.getElementById('searchUrl');
+const generateButton = document.getElementById('generateQrCode');
+const downloadButton = document.getElementById('downloadQrCode');
+const openButton = document.getElementById('openQrCode');
+const deleteButton = document.getElementById('deleteQrCode');
+const qrCodeImage = document.getElementById('qrCodeImage');
+
 document.addEventListener('DOMContentLoaded', qrCodeGenerator);
 document.addEventListener('DOMContentLoaded', backgroundMusic);
 
@@ -18,14 +26,43 @@ function backgroundMusic() {
 }
 
 function qrCodeGenerator() {
-  const input = document.getElementById('searchUrl');
-  const generateButton = document.getElementById('generateQrCode');
-  const qrCodeImage = document.getElementById('qrCodeImage');
-
-  generateButton.addEventListener('click', async () => {
-    const dataUrl = await QRCode.toDataURL(input.value);
-    qrCodeImage.src = dataUrl;
+  if (counter == 0) {
+    qrCodeImage.src = 'assets/icons/question.png';
     qrCodeImage.style.display = 'block';
-    input.value = '';
+  }
+  generateButton.addEventListener('click', async () => {
+    if (!input.value.trim()) {
+      counter++;
+      switch (counter) {
+        case 1:
+          input.placeholder = 'PLEASE ENTER A URL';
+          break;
+        case 2:
+          input.placeholder = 'JUST A URL, PLEASE';
+          break;
+        case 3:
+          input.placeholder = 'REALLY, A URL WOULD BE NICE';
+          break;
+        case 4:
+          input.placeholder = 'I NEED A URL TO WORK WITH';
+          break;
+        default:
+          input.placeholder = 'OK, I GIVE UP';
+          counter = 0;
+          break;
+      }
+    if (counter > 0) {
+      qrCodeImage.src = "assets/icons/wrong.png";
+      qrCodeImage.style.display = 'block';
+    }
+    }
+    if (input.value.trim()) {
+      const dataUrl = await QRCode.toDataURL(input.value);
+      qrCodeImage.src = dataUrl;
+      qrCodeImage.style.display = 'block';
+      input.value = '';
+      counter = 0;
+      input.placeholder = "ENTER A URL";
+    }
   });
 }
